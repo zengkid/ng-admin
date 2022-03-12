eb87b007149522e9a1218691d1bde0d56fbcfb49
zengkid <zengkid@msn.com>
2022-03-12 19:10:11 +0800

    redirect admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-admin';
+
+  constructor(
+    private router: Router,
+    private activeRoute: ActivatedRoute
+ ) { 
+    this.router.navigate(['/admin']);
+    // this.router.navigate()
+  }
 }
