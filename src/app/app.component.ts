728edc484ecb8ffc51416ea37aa022db50c84fbf
zengkid <zengkid@msn.com>
2022-03-13 21:59:44 +0800

    add fontanesome icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NbIconLibraries } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,16 @@ export class AppComponent {
   title = 'ng-admin';
 
   constructor(
+    private iconLibraries: NbIconLibraries,
     private router: Router,
     private activeRoute: ActivatedRoute
- ) { 
-    this.router.navigate(['/admin']);
-    // this.router.navigate()
+  ) {
+    this.iconLibraries.registerFontPack('fa', { packClass: 'fa', iconClassPrefix: 'fa' });
+    this.iconLibraries.registerFontPack('fas', { packClass: 'fa-solid', iconClassPrefix: 'fa' });
+    this.iconLibraries.registerFontPack('far', { packClass: 'fa-regular', iconClassPrefix: 'fa' });
+    // this.iconLibraries.registerFontPack('fal', { packClass: 'fa-light', iconClassPrefix: 'fa' });
+    // this.iconLibraries.registerFontPack('fat', { packClass: 'fa-thin', iconClassPrefix: 'fa' });
+    // this.iconLibraries.registerFontPack('fad', { packClass: 'fa-duotone', iconClassPrefix: 'fa' });
+    // this.iconLibraries.setDefaultPack('fa');// <---- set as default
   }
 }
