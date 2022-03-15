9ed89d9f4606b2fc9479a7ab716955b193f35153
zengkid <zengkid@msn.com>
2022-03-15 16:19:57 +0800

    auth login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { NbPasswordAuthStrategy, NbAuthModule } from '@nebular/auth';
+import { NbPasswordAuthStrategy, NbAuthModule, NbAuthJWTToken } from '@nebular/auth';
 import { NbActionsModule, NbIconModule, NbLayoutModule, NbMenuModule, NbSidebarModule, NbThemeModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { AppRoutingModule } from './app-routing.module';
@@ -28,6 +28,20 @@ import { AppComponent } from './app.component';
       strategies: [
         NbPasswordAuthStrategy.setup({
           name: 'email',
+          login: {
+            // endpoint: '/auth/login',
+            method: 'post',
+            // alwaysFail: true,
+            requireValidToken: true,
+            redirect: {
+              success: '/admin',
+              failure: null,
+            }
+          },
+          token: {
+            key: 'token',
+            class: NbAuthJWTToken,
+          },
         }),
       ],
       forms: {},
